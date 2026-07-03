e00359234808ace776b16d53a0b724e98a594411
Neto Frazão <frazaoneto307@gmail.com>
2026-07-03 19:34:43 -0300

    fix base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/rede-social-eteepa/",
+  base: '/',   // 👈 IMPORTANTE
   plugins: [react(), tailwindcss()],
 })
